aad22f01b306f53c7d2cb2556d0ace54646345dc
David Egwuatu <davidegwuatu2@gmail.com>
2022-11-14 15:54:43 +0100

    cd handler

--- f_chdir.c
+++ f_chdir.c
@@ -2,4 +2,19 @@
 
 int f_chdir(char **arg, char **env)
 {
+	char **wd;
+	char *path, *f_sl = "/";
+	int i = 0;
+
+	while (env[i] != NULL)
+	{
+		wd = _strtow(env[i], '=');
+		if (_strcmp(wd[0], "HOME") == 0)
+		{
+			path = wd[1];
+			break;
+		}
+		i++;
+	}
+
 }
