1b8004075852fa42753e25d73905d601b4708869
Nnamani John <nnamanijoh123@gmail.com>
2022-11-16 15:34:48 +0000

    adjust task 1

--- main.h
+++ main.h
@@ -13,39 +13,12 @@
 #include <errno.h>
 #include <stdbool.h>
 
-/**
- * struct builtin_cmd - structure to handle builtin command
- * @cmd: command argument
- * @func: pointer to builtin command function
- */
-typedef struct builtin_cmd
-{
-	char *cmd;
-	int (*func)();
-} builtin_cmd;
-
 struct flags
 {
 	bool interactive;
 } flags;
 
-int _putchar(char c);
 void prompt(void);
 char **_strtow(char *str, char delim);
-int _strlen(char *str);
-char *_strncpy(char *dest, char *src, int n);
-char *_strcat(char *dest, char *src);
-char *ch_path(char *cmd, char *path);
-int gbin_func(char **arg, char **env);
-int f_exit(char **arg);
-int _strcmp(char *s1, char *s2);
-int f_printenv(char **arg, char **env);
-void full_path(char **argv, char **env);
-int exe_syscmd(char **argv, char **env);
-int exe_cmd(char *buf, char **env);
-void cmd_sep(char *buf, char **env);
-int _atoi(char *s);
-int f_chdir(char **arg, char **env);
-char *_getenv(char **env, char *var);
 
 #endif /* MAIN_H */
